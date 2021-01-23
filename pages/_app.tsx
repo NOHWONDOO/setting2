0ea548c6f2e495c306a87c190e6dcc98867580c8
NOHWONDOO <nwwd0907@naver.com>
2021-01-23 18:37:40 +0900

    123

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import { AppProps } from "next/dist/next-server/lib/router/router";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  asdfasdfsadfasdf;
   return <Component {...pageProps} />;
 }
 
